1df8b5294895320712d40dc2114378d307807f16
Power Up Studio <76599999+PowerUpStudios@users.noreply.github.com>
2023-05-13 22:26:48 +0200

    Update imgen.js

--- imgen.js
+++ imgen.js
@@ -2,11 +2,10 @@ PGPTConversation.onMessage((msg) => {
   const ics = msg.container.getElementsByTagName("imgen-c")
   for (let ic = 0; ic < ics.length; ic++) {
     if (ics[ic].getAttribute("keyword")) {
-      console.log(ics[ic].getAttribute("keyword"))
-      fetch(`https://en.wikipedia.org/w/api.php?action=query&titles=${ics[ic].getAttribute("keyword")}&prop=pageimages&format=json&pithumbsize=100`).then(res => res.json()).then((res) => {
-        let url = res.query.pages[Object.keys(res.query.pages)[0]].thumbnail
-        ics[ic].outerHTML = `<img src="${url}></img>`
-      })
+      fetch("https://server.powerupstudio.eu/access", {headers: {"x-access-url": `https://en.wikipedia.org/w/api.php?action=query&titles=${ics[ic].getAttribute("keyword")}&prop=pageimages&format=json&pithumbsize=100`}}).then(res => res.json()).then((res) => {
+          let url = res.query.pages[Object.keys(res.query.pages)[0]].thumbnail
+          ics[ic].outerHTML = `<img src="${url}></img>`
+        })
     }
   }
   msg.contentChanged(null, () => {
