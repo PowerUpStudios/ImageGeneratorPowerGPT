823694e41f160b0771ddb51818c82bc20b242230
Power Up Studio <76599999+PowerUpStudios@users.noreply.github.com>
2023-05-13 23:08:09 +0200

    Update imgen.js

--- imgen.js
+++ imgen.js
@@ -4,7 +4,7 @@ PGPTConversation.onMessage((msg) => {
     if (ics[ic].getAttribute("keyword")) {
       fetch("https://server.powerupstudio.eu/access", {headers: {"x-access-url": `https://en.wikipedia.org/w/api.php?action=query&titles=${ics[ic].getAttribute("keyword")}&prop=pageimages&format=json&pithumbsize=100`}}).then(res => res.json()).then((res) => {
           console.log(JSON.parse(res.res))
-          let url = JSON.parse(res.res).query.pages[Object.keys(res.res.query.pages)[0]].thumbnail
+          let url = JSON.parse(res.res).query.pages[Object.keys(JSON.parse(res.res).query.pages)[0]].thumbnail
           ics[ic].outerHTML = `<img src="${url}></img>`
         })
     }
@@ -15,7 +15,7 @@ PGPTConversation.onMessage((msg) => {
       if (ics[ic].getAttribute("keyword")) {
         fetch("https://server.powerupstudio.eu/access", {headers: {"x-access-url": `https://en.wikipedia.org/w/api.php?action=query&titles=${ics[ic].getAttribute("keyword")}&prop=pageimages&format=json&pithumbsize=100`}}).then(res => res.json()).then((res) => {
           console.log(JSON.parse(res.res))
-          let url = JSON.parse(res.res).query.pages[Object.keys(res.res.query.pages)[0]].thumbnail
+          let url = JSON.parse(res.res).query.pages[Object.keys(JSON.parse(res.res).query.pages)[0]].thumbnail
           ics[ic].outerHTML = `<img src="${url}></img>`
         })
       }
