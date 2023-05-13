4085c3f7cd4ed291a0b08ea49138d1de9b085795
Power Up Studio <76599999+PowerUpStudios@users.noreply.github.com>
2023-05-13 22:10:04 +0200

    Update imgen.js

--- imgen.js
+++ imgen.js
@@ -1,4 +1,22 @@
 PGPTConversation.onMessage((msg) => {
-  console.log(msg)
+  const ics = msg.container.getElementsByTagName("imgen-c")
+  for (let ic = 0; ic < ics.length; ic++) {
+    if (ics[ic].getAttribute("keyword")) {
+      fetch(`https://en.wikipedia.org/w/api.php?action=query&titles=${ics[ic].getAttribute("keyword")}&prop=pageimages&format=json&pithumbsize=100`).then(res => res.json()).then((res) => {
+        let url = res.query.pages[Object.keys(res.query.pages)[0]].thumbnail
+        ics[ic].outerHTML = `<img src="${url}></img>`
+      })
+    }
+  }
+  msg.contentChanged(null, () => {
+    const ics = msg.container.getElementsByTagName("imgen-c")
+    for (let ic = 0; ic < ics.length; ic++) {
+      if (ics[ic].getAttribute("keyword")) {
+        fetch(`https://en.wikipedia.org/w/api.php?action=query&titles=${ics[ic].getAttribute("keyword")}&prop=pageimages&format=json&pithumbsize=100`).then(res => res.json()).then((res) => {
+          let url = res.query.pages[Object.keys(res.query.pages)[0]].thumbnail
+          ics[ic].outerHTML = `<img src="${url}></img>`
+        })
+      }
+    }
+  })
 }, window.parent.document)
-console.log("UwU")
